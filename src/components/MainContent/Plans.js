a5a4bfe0e98177cc7cf719125869788ea588fab0
Mohamed Younis <170289648+MohaDev01@users.noreply.github.com>
2024-08-31 12:09:11 +0300

    buttons and icons position updated

--- src/components/MainContent/Plans.js
+++ src/components/MainContent/Plans.js
@@ -6,10 +6,10 @@ function Plans () {
         <div className="col-md-12">
           <p>Start Creating Unique Digital Products for Free</p>
           <p>Our Free Plan allows you to: 1. Access unlimited design resources 2. Choose from 3,000+ templates 3. Begin engaging your audience</p>
-          <button className="btn btn-dark px-5 py-2 mx-4">
+          <button className="btn btn-dark" aria-label='Sign up'>
             Sign up
           </button>
-          <button className="btn btn-outline-dark px-4 py-2">
+          <button className="btn btn-outline-dark" aria-label='Compare plans'>
             Compare plans
           </button>
         </div>
